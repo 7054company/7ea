1186a37067247bc3e086d0822bb52ba71fb95b95
7054company <7054company@gmail.com>
2019-01-07 21:09:20 +0530

    update

--- source/admin/previews.js
+++ source/admin/previews.js
@@ -70,7 +70,7 @@ const NavigationPreview = ({ entry }) => {
     <header className="header-primary">
       <div className="logo">
         <a href="/">
-          <img src="/images/logo.png" alt="7ea"/>
+          <img src="/images/logo.jpg" alt="7ea"/>
         </a>
       </div>
       <a className="nav-button">
